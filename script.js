681ad359d10949ea955173def07b54ddf9a86789
Leticia Lima <leticia.l.silva@outlook.com>
2020-08-20 21:39:36 -0300

    definir o fim do jogo

--- script.js
+++ script.js
@@ -26,7 +26,9 @@ function criarCobrinha(){
 function drawFood(){
     context.fillStyle = "red";
     context.fillRect(food.x, food.y, box, box);
+
 }
+
 document.addEventListener('keydown', update);
 
 function update (event){
@@ -42,6 +44,13 @@ function iniciarJogo(){
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake [0].y = 16 * box;
+
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Game Over :(');
+        }
+    }
 
     criarBG();
     criarCobrinha();
